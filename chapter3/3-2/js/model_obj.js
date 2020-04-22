8836f213e160b1856b079ed7134c52a5531a28f9
田﨑貴大 <qxgxyea_h@yahoo.co.jp>
2020-04-22 13:28:35 +0900

    オブジェクトのバインド

---
+++ chapter3/3-2/js/model_obj.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    unit: {}
+  },
+  methods: {
+    onchange: function() {
+      console.log(this.unit.name + "：" + this.unit.size);
+    }
+  }
+});
