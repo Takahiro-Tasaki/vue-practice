7eaacc2bb3f2ef791d89d3b31e761a5492fa1d80
田﨑貴大 <qxgxyea_h@yahoo.co.jp>
2020-04-22 14:55:21 +0900

    templateの利用

---
+++ chapter3/3-3/js/for_multi.js
@@ -0,0 +1,27 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    songs: [
+      {
+        title: "赤とんぼ",
+        lyrics: "夕焼け小焼けの赤とんぼ",
+        composer: "山田耕作"
+      },
+      {
+        title: "赤とんぼ",
+        lyrics: "夕焼け小焼けの赤とんぼ",
+        composer: "山田耕作"
+      },
+      {
+        title: "赤とんぼ",
+        lyrics: "夕焼け小焼けの赤とんぼ",
+        composer: "山田耕作"
+      },
+      {
+        title: "赤とんぼ",
+        lyrics: "夕焼け小焼けの赤とんぼ",
+        composer: "山田耕作"
+      }
+    ]
+  }
+});
